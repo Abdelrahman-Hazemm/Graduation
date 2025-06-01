8141351d1931f079d1eb0fd86a294e9b31172e28
Abd El Rahman Hazem <boshahazem123@gmail.com>
2025-06-02 00:21:14 +0300

    .

--- ru2ya/lib/pages/info.dart
+++ ru2ya/lib/pages/info.dart
@@ -41,156 +41,164 @@ class Info extends StatelessWidget {
               ),
               child: const Padding(
                 padding: EdgeInsets.all(20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(height: 10),
-                    Center(
-                      child: Text(
-                        "Personal Information",
-                        style: TextStyle(
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 10),
+                      Center(
+                        child: Text(
+                          "Personal Information",
+                          style: TextStyle(
                             fontSize: 28,
                             fontWeight: FontWeight.bold,
-                            color: Colors.blue),
+                            color: Colors.blue,
+                          ),
+                        ),
                       ),
-                    ),
-                    SizedBox(height: 40),
-                    Padding(
-                      padding: EdgeInsets.only(left: 10),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Name',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                              color: Colors.blue,
+                      SizedBox(height: 40),
+                      Padding(
+                        padding: EdgeInsets.only(left: 10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Name',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                color: Colors.blue,
+                              ),
                             ),
-                          ),
-                          SizedBox(height: 20.0),
-                          Text(
-                            'Jane',
-                            style: TextStyle(
-                              color: Colors.black54,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 15
-                            ),
-                          ),
-                          SizedBox(height: 10.0),
-                        ],
-                      ),
-                    ),
-                    Divider(thickness: 1, color: Colors.black),
-                    Padding(
-                      padding: EdgeInsets.only(left: 10),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          SizedBox(height: 10.0),
-                          Text(
-                            'Glass ID',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                              color: Colors.blue,
-                            ),
-                          ),
-                          SizedBox(height: 20.0),
-                          Text(
-                            '#001',
-                            style: TextStyle(
+                            SizedBox(height: 20.0),
+                            Text(
+                              'Jane',
+                              style: TextStyle(
                                 color: Colors.black54,
                                 fontWeight: FontWeight.bold,
-                                fontSize: 15
-                            ),                          ),
-                          SizedBox(height: 10.0),
-                        ],
-                      ),
-                    ),
-                    Divider(thickness: 1, color: Colors.black),
-                    Padding(
-                      padding: EdgeInsets.only(left: 10),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          SizedBox(height: 10.0),
-                          Text(
-                            'Phone Number',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                              color: Colors.blue,
+                                fontSize: 15,
+                              ),
                             ),
-                          ),
-                          SizedBox(height: 20.0),
-                          Text(
-                            '01000000000',
-                            style: TextStyle(
+                            SizedBox(height: 10.0),
+                          ],
+                        ),
+                      ),
+                      Divider(thickness: 1, color: Colors.black),
+                      Padding(
+                        padding: EdgeInsets.only(left: 10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(height: 10.0),
+                            Text(
+                              'Glass ID',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                color: Colors.blue,
+                              ),
+                            ),
+                            SizedBox(height: 20.0),
+                            Text(
+                              '#001',
+                              style: TextStyle(
                                 color: Colors.black54,
                                 fontWeight: FontWeight.bold,
-                                fontSize: 15
-                            ),                          ),
-                          SizedBox(height: 10.0),
-                        ],
-                      ),
-                    ),
-                    Divider(thickness: 1, color: Colors.black),
-                    Padding(
-                      padding: EdgeInsets.only(left: 10),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          SizedBox(height: 10.0),
-                          Text(
-                            'Date Of Birth',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                              color: Colors.blue,
+                                fontSize: 15,
+                              ),
                             ),
-                          ),
-                          SizedBox(height: 20.0),
-                          Text(
-                            '30 May 2003',
-                            style: TextStyle(
+                            SizedBox(height: 10.0),
+                          ],
+                        ),
+                      ),
+                      Divider(thickness: 1, color: Colors.black),
+                      Padding(
+                        padding: EdgeInsets.only(left: 10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(height: 10.0),
+                            Text(
+                              'Phone Number',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                color: Colors.blue,
+                              ),
+                            ),
+                            SizedBox(height: 20.0),
+                            Text(
+                              '01000000000',
+                              style: TextStyle(
                                 color: Colors.black54,
                                 fontWeight: FontWeight.bold,
-                                fontSize: 15
-                            ),                          ),
-                          SizedBox(height: 10.0),
-                        ],
-                      ),
-                    ),
-                    Divider(thickness: 1, color: Colors.black),
-                    Padding(
-                      padding: EdgeInsets.only(left: 10),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          SizedBox(height: 10.0),
-                          Text(
-                            'Gender',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                              color: Colors.blue,
+                                fontSize: 15,
+                              ),
                             ),
-                          ),
-                          SizedBox(height: 20.0),
-                          Text(
-                            'Female',
-                            style: TextStyle(
+                            SizedBox(height: 10.0),
+                          ],
+                        ),
+                      ),
+                      Divider(thickness: 1, color: Colors.black),
+                      Padding(
+                        padding: EdgeInsets.only(left: 10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(height: 10.0),
+                            Text(
+                              'Date Of Birth',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                color: Colors.blue,
+                              ),
+                            ),
+                            SizedBox(height: 20.0),
+                            Text(
+                              '30 May 2003',
+                              style: TextStyle(
                                 color: Colors.black54,
                                 fontWeight: FontWeight.bold,
-                                fontSize: 15
-                            ),                          ),
-                          SizedBox(height: 10.0),
-                        ],
+                                fontSize: 15,
+                              ),
+                            ),
+                            SizedBox(height: 10.0),
+                          ],
+                        ),
                       ),
-                    ),
-                    Divider(thickness: 1, color: Colors.black),
-                  ],
+                      Divider(thickness: 1, color: Colors.black),
+                      Padding(
+                        padding: EdgeInsets.only(left: 10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(height: 10.0),
+                            Text(
+                              'Gender',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                color: Colors.blue,
+                              ),
+                            ),
+                            SizedBox(height: 20.0),
+                            Text(
+                              'Female',
+                              style: TextStyle(
+                                color: Colors.black54,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15,
+                              ),
+                            ),
+                            SizedBox(height: 10.0),
+                          ],
+                        ),
+                      ),
+                      Divider(thickness: 1, color: Colors.black),
+                      SizedBox(height: 30),
+                    ],
+                  ),
                 ),
               ),
             ),
